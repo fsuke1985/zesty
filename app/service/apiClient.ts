52edf1f33e6748cc84415e7ac18194dbfe136b33
fsuke1985 <fsuke1985@users.noreply.github.com>
2022-07-17 12:57:56 +0000

    add api Route

--- app/service/apiClient.ts
+++ app/service/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: '/api/v1',
+    // baseURL: '/api/',
     timeout: 1000,
     headers: {'X-Requested-With': 'XMLHttpRequest'},
     withCredentials: true
