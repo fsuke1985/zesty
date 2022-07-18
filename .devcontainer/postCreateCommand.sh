36857e94f10ac1cc32d84fde27dff1cb8f49420f
2020mini.fsuke1985 <fsuke1985@gmail.com>
2022-07-18 04:01:26 +0000

    mod devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,4 +5,8 @@ docker-compose build && docker-compose up -d
 yes | gcloud auth login --cred-file=<(echo $GCP_CREDENTIALS)
 gcloud config set project $GCP_PROJECTID
 
+wget -qO- https://get.pnpm.io/install.sh | bash - \
+    && export PNPM_HOME="/home/vscode/.local/share/pnpm" \
+    && export PATH="$PNPM_HOME:$PATH"
+    
 exit 0
