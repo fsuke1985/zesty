7beeeb35589df30822905dfadde40aab4372a78f
fsuke1985 <fsuke1985@gmail.com>
2022-07-30 10:26:59 +0900

    initial commit

---
+++ src/client/hooks/useUser.tsx
@@ -0,0 +1,6 @@
+import React, { createContext, useContext, useState } from 'react'
+import { useRouter } from 'next/router'
+
+export const useUser = () => {
+
+}
--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import "../styles/auth.scss"
+import { useUser } from '~/hooks'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
